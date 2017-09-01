e5b77a330c889df921e4da32aec1bd4d6617e310
Clay Smith <clayton.smith@macquarie.com>
2017-09-01 16:35:21 -0500

    Add example list for tests

--- src/test/java/ListSearcherTests.java
+++ src/test/java/ListSearcherTests.java
@@ -3,14 +3,28 @@ import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.List;
 
 public class ListSearcherTests {
 
     @Test
     public void testSearch() throws IOException {
-        ListSearcher lists = new ListSearcher(new File("lists"));
+
+
+        File listDirectory = getListsDirectory();
+        ListSearcher lists = new ListSearcher(listDirectory);
         List<String> metallica = lists.search("^\\!.*Metallica.*");
         Assert.assertFalse(metallica.isEmpty());
+    }
+
+    private File getListsDirectory() {
+        try {
+            URL listsDirectoryUrl = ListSearcher.class.getResource("/lists");
+            return new File(listsDirectoryUrl.toURI());
+        } catch (URISyntaxException e) {
+            throw new RuntimeException("Failed to load lists directory", e);
+        }
     }
 }
