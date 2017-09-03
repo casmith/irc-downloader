3da94f299d90fdaa7964478f401a9adbd14a8284
Clay Smith <uhclay@gmail.com>
2017-09-03 09:38:39 -0500

    Add DownloadQueueRepository
    Including default implementation using a simple flat file

---
+++ src/test/java/FlatFileDownloadQueueRepositoryTests.java
@@ -0,0 +1,35 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class FlatFileDownloadQueueRepositoryTests {
+
+
+    private FlatFileDownloadQueueRepository repository;
+
+    @Before
+    public void setUp() {
+        this.repository = new FlatFileDownloadQueueRepository("test-queue.dat");
+    }
+
+    @Test
+    public void testSaveAndList() {
+        // save the queue
+        List<FileRequest> queue = Arrays.asList(
+                new FileRequest("!username filename1.mp3"),
+                new FileRequest("!username filename2.mp3"),
+                new FileRequest("!username filename3.mp3")
+        );
+        this.repository.save(queue);
+
+        // load the queue again
+        List<FileRequest> list = this.repository.list();
+
+        // they should match
+        assertEquals(queue, list);
+    }
+}
