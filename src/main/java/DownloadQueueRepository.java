5d0300c2acd92fbdaa8c9df48ec0afc4abb0b931
Clay Smith <uhclay@gmail.com>
2019-08-06 08:48:27 -0500

    Remove attribution comment

--- src/main/java/DownloadQueueRepository.java
+++ src/main/java/DownloadQueueRepository.java
@@ -1,8 +1,5 @@
 import java.util.Queue;
 
-/**
- * Created by clay on 9/3/17.
- */
 public interface DownloadQueueRepository {
     Queue<FileRequest> list();
     void save(Queue<FileRequest> downloadQueue);
