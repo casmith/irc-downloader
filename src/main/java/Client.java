2fefbefaa372eae5fe9e2ce0ba0c65f02b041dfc
= <clayton.smith@macquarie.com>
2019-08-07 00:47:35 -0500

    Add example advert comment for future development

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -3,6 +3,11 @@ import com.typesafe.config.ConfigFactory;
 import marvin.irc.IrcBot;
 import marvin.irc.IrcBotImpl;
 
+/*
+ * Example list string:
+ * Type: @Key-Lay for my list of 3189 files (16.2GB) Updated: Aug 2nd == Free Slots: 6/6 == Files in Que: 0 == Total Speed: 0cps == Next Open Slot: NOW == Files served: 471825 == Using SDFind v3.99e
+ */
+
 public class Client {
     public static void main(String[] args) {
 
