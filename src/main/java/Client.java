39e35a0651b9b6ffd4c02a86db8133c904aee0c1
Clay Smith <uhclay@gmail.com>
2019-08-10 11:39:20 -0700

    Rename channel to controlChannel

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -33,8 +33,8 @@ public class Client {
                 ircConfig.getInt("port"),
                 ircConfig.getString("nick"),
                 password,
-                ircConfig.getString("channel"),
                 ircConfig.getString("adminpw"),
+                ircConfig.getString("controlChannel"),
                 ircConfig.getString("requestChannel"),
                 ircConfig.getString("downloadDirectory"));
 
