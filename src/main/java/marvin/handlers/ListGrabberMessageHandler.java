4da68ae750d1e5dea25934d25c42bae06f51b34e
Clay Smith <casmith@users.noreply.github.com>
2022-02-18 19:38:39 -0800

    Remove stubborn debug logging

--- src/main/java/marvin/handlers/ListGrabberMessageHandler.java
+++ src/main/java/marvin/handlers/ListGrabberMessageHandler.java
@@ -19,12 +19,9 @@ public class ListGrabberMessageHandler implements MessageHandler {
 
     @Override
     public void onMessage(String channelName, String nick, String message, String hostmask) {
-        LOG.debug("{} {}", channelName, message);
-
         if (listGrabber.check(message)) {
             listGrabber.updateLastSeen(nick, message, hostmask);
         }
-
         try {
             if (listGrabber.grab(channelName, message)) {
                 bot.messageControlChannel("Requested list from {0}", nick);
