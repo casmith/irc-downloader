c28454285ca3aa413007641dcf20387c510ed7ce
Clay Smith <casmith@users.noreply.github.com>
2022-07-19 03:09:28 -0700

    Add debug logging endpoint

--- src/main/java/marvin/web/server/ServerResource.java
+++ src/main/java/marvin/web/server/ServerResource.java
@@ -1,6 +1,11 @@
 package marvin.web.server;
 
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.LoggerContext;
 import marvin.irc.IrcBot;
+import org.slf4j.LoggerFactory;
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
 
 import javax.inject.Inject;
 import javax.ws.rs.GET;
@@ -25,5 +30,23 @@ public class ServerResource {
     public Response listUsers() {
         return Response.status(200)
             .entity(this.ircBot.listUsers()).build();
+    }
+
+    @GET
+    @Path("/logging")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response setLogLevel() {
+
+        LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
+
+        Logger root = (Logger)LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
+
+        System.out.println("Current root logger level: " + root.getLevel());
+
+        root.setLevel(Level.DEBUG);
+
+        System.out.println("Current root logger level: " + root.getLevel());
+
+        return Response.status(200).build();
     }
 }
